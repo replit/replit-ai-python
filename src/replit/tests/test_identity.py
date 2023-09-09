07fdc88c57d2601961e1bee6fa3af66c943dd679
Replit user <>
2023-09-09 23:33:42 +0000

    mocked tokens

--- src/replit/tests/test_identity.py
+++ src/replit/tests/test_identity.py
@@ -4,13 +4,14 @@ import json
 import os
 import unittest
 from unittest.mock import patch
-
+from unittest import mock
 import pyseto
 import pytest
 from replit.ai.identity import verify
 from replit.ai.identity.sign import SigningAuthority
 from replit.ai.replit_identity_token_manager import ReplitIdentityTokenManager
-from replit.ai.config import Config
+from replit.ai import config
+from unittest.mock import Mock
 
 PUBLIC_KEY = "on0FkSmEC+ce40V9Vc4QABXSx6TXo+lhp99b6Ka0gro="
 
@@ -25,52 +26,65 @@ PUBLIC_KEY = "on0FkSmEC+ce40V9Vc4QABXSx6TXo+lhp99b6Ka0gro="
 IDENTITY_PRIVATE_KEY = "k2.secret.6sHU27WoRIaspIOVaShpuZM33ozpfFyI2THfO8fmSX6xiA_Duh4ac5g76Y5bParclsalaOCTaCs6gZowhYivVQ"  # noqa: E501,B950,S106 # line too long
 IDENTITY_TOKEN = "v2.public.Q2dSMFpYTjBJZ1IwWlhOMHDnO17Eg43zucAMSAHnCS4C1wn4QUCCOcr-Pggw5SV1KnbOXq8RcQE5if6pMcbJ6lmRWcdoHq5CV9jqyRrUlwo.R0FFaUJtTnZibTFoYmhLckFuWXlMbkIxWW14cFl5NVJNbVF6VTFkd2VHRnRXbmRrTVd4U1lXMDViRm96YkVKU1ZrNUZVVmRzYTA1VmQzSlRSVlp2VWtaa2RGbFZVa3BSVmtwMlVUQmtRbFpYUmtOYU1qbEdXa1ZrVjJWdFVrUlRWRVpvWld0c01Wa3dhRmRoVjBwSVlrZHdUV0pyTldGWGFrWkRUVEEwZVU5WGVGTk5hbFpSVmpGVk5HUkhTbFpQVm1oc1lXdHdORlJVUW5kaFZrbDZVV3hvYUdKWFVubFVWekZyWlZaUmVVOVZhRnBXVkVaTFZtcENjMlZWTVZkV1ZEQkRUbVpoYkhkMk5EUm9SRkZQTFVKWlJDMURWSEUxYVdJeFQzVlVlamxIWW5WTlFVVnFURFExUVVwclZXNW9kR2hxVFZOVVRtOVZSRVphWDBsaVUyTjFjekoxWW05aVowNU1MV2RRVlRGRmVVOTFUVzlHTGxJd1JrWmhWVXAwVkc1YWFXSlVSbTlaYldSMlVteHdTRlpxU2xCaGExVTU"  # noqa: E501,B950,S106 # line too long
 
+
 @pytest.fixture()
 def setup_pub_key():
-        os.environ["REPL_PUBKEYS"] = json.dumps({"dev:1": PUBLIC_KEY})
+  os.environ["REPL_PUBKEYS"] = json.dumps({"dev:1": PUBLIC_KEY})
+
 
 @pytest.mark.usefixtures("setup_pub_key")
 def test_read_public_key_from_env() -> None:
-    """Test read_public_key_from_env."""
-    pubkey = verify.read_public_key_from_env("dev:1", "goval")
-    assert isinstance(pubkey, pyseto.versions.v2.V2Public)
-    
-def test_signing_authority() -> None:
-    """Test SigningAuthority."""
-    gsa = SigningAuthority(
-        marshaled_private_key=IDENTITY_PRIVATE_KEY,
-        marshaled_identity=IDENTITY_TOKEN,
-        replid="test",
-    )
-    signed_token = gsa.sign("audience")
+  """Test read_public_key_from_env."""
+  pubkey = verify.read_public_key_from_env("dev:1", "goval")
+  assert isinstance(pubkey, pyseto.versions.v2.V2Public)
 
-    verify.verify_identity_token(
-        identity_token=signed_token,
-        audience="audience",
-    )
+
+def test_signing_authority() -> None:
+  """Test SigningAuthority."""
+  gsa = SigningAuthority(
+      marshaled_private_key=IDENTITY_PRIVATE_KEY,
+      marshaled_identity=IDENTITY_TOKEN,
+      replid="test",
+  )
+  signed_token = gsa.sign("audience")
+
+  verify.verify_identity_token(
+      identity_token=signed_token,
+      audience="audience",
+  )
+
 
 def test_verify_identity_token() -> None:
-    """Test verify_identity_token."""
-    verify.verify_identity_token(
-        identity_token=IDENTITY_TOKEN,
-        audience="test",
-    )
+  """Test verify_identity_token."""
+  verify.verify_identity_token(
+      identity_token=IDENTITY_TOKEN,
+      audience="test",
+  )
 
-@pytest.fixture()
-def setup_identities():
-  if "REPL_IDENTITY_KEY" not in os.environ:
-      os.environ["REPL_IDENTITY_KEY"] = IDENTITY_PRIVATE_KEY
-  if "REPL_IDENTITY" not in os.environ:     
-      os.environ["REPL_IDENTITY"] = IDENTITY_TOKEN
-  if "REPL_ID" not in os.environ:     
-      os.environ["REPL_ID"] = "test"
-@pytest.mark.usefixtures("setup_identities")
-@patch("replit.ai.config.Config","1232323")
+
+def setup_identities(var_id) -> str:
+  if var_id == "REPL_IDENTITY_KEY":
+    return IDENTITY_PRIVATE_KEY
+  if var_id == "REPL_IDENTITY":
+    return IDENTITY_TOKEN
+  if var_id == "REPL_ID":
+    return "test"
+
+
 def test_get_interactive_token() -> None:
-     replit_identity_token_manager = ReplitIdentityTokenManager()
-     signed_token = replit_identity_token_manager.get_interactive_token()
-     verify.verify_identity_token(
-        identity_token=signed_token,
-        audience='modelfarm@replit.com',
-    )
-     
+  ReplitIdentityTokenManager.get_env_var = Mock()
+  ReplitIdentityTokenManager.get_env_var.side_effect = setup_identities
+  mock_config = Mock(spec=config.Config())
+  mock_config.rootUrl = "https://production-modelfarm.replit.com"
+  mock_config.audience = "mod@replit.com"
+  get_config = Mock()
+  get_config.side_effect = mock_config
+
+  replit_identity_token_manager = ReplitIdentityTokenManager()
+
+  signed_token = replit_identity_token_manager.get_interactive_token()
+
+  verify.verify_identity_token(
+      identity_token=signed_token,
+      audience='modelfarm@replit.com',
+  )
