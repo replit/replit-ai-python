3d0de9cb24b840fc900629fbc965a0bf9bb5725b
Madhav <madhav.singhal@repl.it>
2023-09-10 22:25:01 -0700

    add imports to avoid .struct usage

--- src/replit/ai/google/preview/language_models/__init__.py
+++ src/replit/ai/google/preview/language_models/__init__.py
@@ -1,2 +1,8 @@
-from .chat_model import (ChatModel as ChatModel, InputOutputTextPair as
-                         InputOutputTextPair)
+from .chat_model import (ChatModel as ChatModel, 
+                         InputOutputTextPair as
+                         InputOutputTextPair, 
+                         ChatMessage as ChatMessage,
+                         ChatSession as ChatSession,
+                         )
+
+from src.replit.ai.google.structs import TextGenerationResponse as TextGenerationResponse
