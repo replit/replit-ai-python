0c6e9674727917b3d0b890b44b8eec1705eca17f
madhavatreplit <madhav.singhal@repl.it>
2023-09-11 21:20:45 -0700

    Add optionals and defaults to structs (#24)

--- src/replit/ai/google/language_models/text_generation_model.py
+++ src/replit/ai/google/language_models/text_generation_model.py
@@ -6,114 +6,115 @@ from replit.ai.google.structs import TextGenerationResponse
 
 
 class TextGenerationModel:
-  """
-  Class representing a Google completion model.
-
-  Methods:
-      from_pretrained - Loads a pretrained model using its identifier
-      predict - completes a human-like text given an initial prompt.
-      async_predict - Async version of the predict method.
-  """
-
-  def __init__(self, model_id: str):
-    """Constructor method to initialize a text generation model."""
-    self.underlying_model = CompletionModel(model_id)
-
-  @staticmethod
-  def from_pretrained(model_id: str) -> 'TextGenerationModel':
     """
-    Creates a Tokenizer from a pretrained model.
+    Class representing a Google completion model.
 
-    Args:
-        model_id (str): The identifier of the pretrained model.
-
-    Returns:
-        The TextGenerationModel class instance.
+    Methods:
+        from_pretrained - Loads a pretrained model using its identifier
+        predict - completes a human-like text given an initial prompt.
+        async_predict - Async version of the predict method.
     """
-    return TextGenerationModel(model_id)
 
-  def predict(self, prompt: str, **kwargs) -> TextGenerationResponse:
-    """
-    completes a human-like text given an initial prompt.
+    def __init__(self, model_id: str):
+        """Constructor method to initialize a text generation model."""
+        self.underlying_model = CompletionModel(model_id)
 
-    Args:
-        prompt (str): The initial text to start the generation.
+    @staticmethod
+    def from_pretrained(model_id: str) -> "TextGenerationModel":
+        """
+        Creates a Tokenizer from a pretrained model.
 
-    Returns:
-        TextGenerationResponse: The model's response containing the completed text.
-    """
-    parameters = ready_parameters(kwargs)
-    response = self.underlying_model.complete([prompt], **parameters)
-    return self.__ready_response(response)
+        Args:
+            model_id (str): The identifier of the pretrained model.
 
-  def predict_streaming(self, prompt: str,
-                        **kwargs) -> Iterator[TextGenerationResponse]:
-    """
-    completes a human-like text given an initial prompt.
+        Returns:
+            The TextGenerationModel class instance.
+        """
+        return TextGenerationModel(model_id)
 
-    Args:
-        prompt (str): The initial text to start the generation.
+    def predict(self, prompt: str, **kwargs) -> TextGenerationResponse:
+        """
+        completes a human-like text given an initial prompt.
 
-    Returns:
-        TextGenerationResponse: The model's response containing the completed text.
-    """
-    parameters = ready_parameters(kwargs)
-    response = self.underlying_model.stream_complete([prompt], **parameters)
-    for x in response:
-      yield self.__ready_response(x)
+        Args:
+            prompt (str): The initial text to start the generation.
 
-  async def async_predict(self, prompt: str,
-                          **kwargs) -> TextGenerationResponse:
-    """
-    Async version of the predict method. Equivalent to the predict method, but suited for asynchronous programming.
+        Returns:
+            TextGenerationResponse: The model's response containing the completed text.
+        """
+        parameters = ready_parameters(kwargs)
+        response = self.underlying_model.complete([prompt], **parameters)
+        return self.__ready_response(response)
 
-    Args:
-        prompt (str): The initial text to start the generation.
+    def predict_streaming(
+        self, prompt: str, **kwargs
+    ) -> Iterator[TextGenerationResponse]:
+        """
+        completes a human-like text given an initial prompt.
 
-    Returns:
-        TextGenerationResponse: The model's response containing the completed text.
-    """
-    parameters = ready_parameters(kwargs)
-    response = await self.underlying_model.async_complete([prompt], **parameters)
-    return self.__ready_response(response)
+        Args:
+            prompt (str): The initial text to start the generation.
 
-  async def async_predict_streaming(self, prompt: str,
-                                    **kwargs) -> TextGenerationResponse:
-    """
-    Async version of the predict method. Equivalent to the predict method, but suited for asynchronous programming.
+        Returns:
+            TextGenerationResponse: The model's response containing the completed text.
+        """
+        parameters = ready_parameters(kwargs)
+        response = self.underlying_model.stream_complete([prompt], **parameters)
+        for x in response:
+            yield self.__ready_response(x)
 
-    Args:
-        prompt (str): The initial text to start the generation.
+    async def async_predict(self, prompt: str, **kwargs) -> TextGenerationResponse:
+        """
+        Async version of the predict method. Equivalent to the predict method, but suited for asynchronous programming.
 
-    Returns:
-        TextGenerationResponse: The model's response containing the completed text.
-    """
-    parameters = ready_parameters(kwargs)
-    response = self.underlying_model.async_stream_complete([prompt], **parameters)
-    async for x in response:
-      yield self.__ready_response(x)
+        Args:
+            prompt (str): The initial text to start the generation.
 
-  def __ready_response(
-      self, response: CompletionModelResponse) -> TextGenerationResponse:
-    """
-    Transforms Completion Model's response into a readily usable format.
+        Returns:
+            TextGenerationResponse: The model's response containing the completed text.
+        """
+        parameters = ready_parameters(kwargs)
+        response = await self.underlying_model.async_complete([prompt], **parameters)
+        return self.__ready_response(response)
 
-    Args:
-        response (CompletionModelResponse): The original response from the underlying model.
+    async def async_predict_streaming(
+        self, prompt: str, **kwargs
+    ) -> TextGenerationResponse:
+        """
+        Async version of the predict method. Equivalent to the predict method, but suited for asynchronous programming.
 
-    Returns:
-        TextGenerationResponse: The transformed response.
-    """
-    choice = response.responses[0].choices[0]
-    safetyAttributes = choice.metadata['safetyAttributes']
-    safetyCategories = dict(
-        zip(safetyAttributes['categories'],
-            safetyAttributes['scores'],
-            strict=True))
+        Args:
+            prompt (str): The initial text to start the generation.
 
-    return TextGenerationResponse(
-        is_blocked=safetyAttributes['blocked'],
-        raw_prediction_response=choice.model_dump(),
-        safety_attributes=safetyCategories,
-        text=choice.content,
-    )
+        Returns:
+            TextGenerationResponse: The model's response containing the completed text.
+        """
+        parameters = ready_parameters(kwargs)
+        response = self.underlying_model.async_stream_complete([prompt], **parameters)
+        async for x in response:
+            yield self.__ready_response(x)
+
+    def __ready_response(
+        self, response: CompletionModelResponse
+    ) -> TextGenerationResponse:
+        """
+        Transforms Completion Model's response into a readily usable format.
+
+        Args:
+            response (CompletionModelResponse): The original response from the underlying model.
+
+        Returns:
+            TextGenerationResponse: The transformed response.
+        """
+        choice = response.responses[0].choices[0]
+        safetyAttributes = choice.metadata["safetyAttributes"]
+        safetyCategories = dict(
+            zip(safetyAttributes["categories"], safetyAttributes["scores"], strict=True)
+        )
+
+        return TextGenerationResponse(
+            is_blocked=safetyAttributes["blocked"],
+            raw_prediction_response=choice.model_dump(),
+            safety_attributes=safetyCategories,
+            text=choice.content,
+        )
