01d7302beb50b4362947cd77562a8c736102cf60
Madhav <madhav.singhal@repl.it>
2023-09-11 18:56:34 -0700

    Refactor

---
+++ src/replit/ai/modelfarm/__init__.py
@@ -0,0 +1,15 @@
+from .completion_model import CompletionModel as CompletionModel
+from .chat_model import ChatModel as ChatModel
+from .embed_model import EmbedModel as EmbedModel
+from .structs import (
+    TokenCountMetadata as TokenCountMetadata, Metadata as Metadata,
+    CompletionModelRequest as CompletionModelRequest, Choice as Choice,
+    PromptResponse as PromptResponse, CompletionModelResponse as
+    CompletionModelResponse, ChatMessage as ChatMessage, ChatExample as
+    ChatExample, ChatSession as ChatSession, ChatModelRequest as
+    ChatModelRequest, Candidate as Candidate, ChatPromptResponse as
+    ChatPromptResponse, ChatModelResponse as ChatModelResponse, Embedding as
+    Embedding, EmbeddingMetadata as EmbeddingMetadata, EmbeddingModelRequest as
+    EmbeddingModelRequest, EmbeddingModelResponse as EmbeddingModelResponse)
+
+__version__ = '0.1.0'
