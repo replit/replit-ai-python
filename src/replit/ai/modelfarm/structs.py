baeefcdb2d0ed9a15703161fa59128f56959d1dc
mederka <meder@kamalov.net>
2023-11-01 22:32:25 +0100

    feat: add classes to help handle image generation

--- src/replit/ai/modelfarm/structs.py
+++ src/replit/ai/modelfarm/structs.py
@@ -1,5 +1,5 @@
-from typing import Optional, List, Union, Dict, Any
-from pydantic import BaseModel
+from typing import Optional, List, Union, Dict, Any, Literal
+from pydantic import BaseModel, Field
 
 
 class TokenCountMetadata(BaseModel):
@@ -86,3 +86,58 @@ class EmbeddingModelRequest(BaseModel):
 class EmbeddingModelResponse(BaseModel):
     metadata: Optional[EmbeddingMetadata] = None
     embeddings: List[Embedding]
+
+
+class ImageGenerationModelImageResult(BaseModel):
+    url: str
+    content_type: str
+    file_name: str
+    file_size: int
+    width: int
+    height: int
+
+
+class ImageGenerationModelResponse(BaseModel):
+    images: List[ImageGenerationModelImageResult]
+    seed: int
+
+
+class ImageSize(BaseModel):
+    width: int = Field(
+        default=512, description="The width of the generated image.", gt=0, le=4096
+    )
+    height: int = Field(
+        default=512, description="The height of the generated image.", gt=0, le=4096
+    )
+
+
+ImageSizePreset = Literal[
+    "square_hd",
+    "square",
+    "portrait_4_3",
+    "portrait_16_9",
+    "landscape_4_3",
+    "landscape_16_9",
+]
+
+
+ImageSizeInput = Union[ImageSize, ImageSizePreset]
+
+
+class LoraWeight(BaseModel):
+    path: str = Field(
+        description="URL or the path to the LoRA weights.",
+        examples=[
+            "https://civitai.com/api/download/models/135931",
+            "https://filebin.net/3chfqasxpqu21y8n/my-custom-lora-v1.safetensors",
+        ],
+    )
+    scale: float = Field(
+        default=1.0,
+        description="""
+            The scale of the LoRA weight. This is used to scale the LoRA weight
+            before merging it with the base model.
+        """,
+        ge=0.0,
+        le=1.0,
+    )
